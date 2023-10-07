0d23f6e43b12216d0d90674d192fcf4b664c11ff
tomcoolpxl <71908551+tomcoolpxl@users.noreply.github.com>
2023-10-07 16:40:47 +0200

    last fixes

--- modules-1/modules/services/webserver-cluster/main.tf
+++ modules-1/modules/services/webserver-cluster/main.tf
@@ -1,6 +1,6 @@
 
 ############################################################################
-# backend config removed for module
+# backend config has no place in a module
 ############################################################################
 
 /*
@@ -8,6 +8,18 @@ terraform {
   backend "s3" { }
 }
 */
+
+############################################################################
+# local variables "locals"
+############################################################################
+
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+}
 
 ############################################################################
 # data
@@ -53,21 +65,26 @@ resource "aws_security_group" "instance" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 # instances
@@ -123,7 +140,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   # By default, return a simple 404 page
--- modules-1/production/services/webserver-cluster/main.tf
+++ modules-1/production/services/webserver-cluster/main.tf
@@ -26,3 +26,20 @@ module "webserver_cluster" {
   min_size      = 2
   max_size      = 10
 }
+
+resource "aws_autoscaling_schedule" "scale_out_in_morning" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
